24cf78dc533ba449f324eeca1242d50099f06e3a
vasudesigns <vishakha.sureja@gmail.com>
2025-08-13 23:57:10 -0500

    added jquery and smooth-scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 console.log("Hi, I’ m Vishakha, I'm a UX/UI designer from Austin, TX!");
+
+$('a').smoothScroll();
+$('#about-picture').hide();
+
+$("#animation-btn").on("click", function () {
+    $('#about-picture').show();
+});
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
