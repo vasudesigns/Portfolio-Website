3ba7ca1a0f0cc3512ee2d5254dac78ecfe24ef11
vasudesigns <vishakha.sureja@gmail.com>
2025-09-18 00:49:45 -0500

    Card flipping animation

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,39 @@ $('a').smoothScroll();
 $('#about-picture').hide();
 
 $("#animation-btn").on("click", function () {
-    $('#about-picture').show();
+  $('#about-picture').show();
 });
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
+  var x = document.getElementById('myNavtoggle');
 
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+setFlipCardHeight();
+
+document.querySelectorAll('.flip-card').forEach(card => {
+  card.addEventListener('click', function () {
+    card.classList.toggle('flipped');
+    setTimeout(setFlipCardHeight, 400);
+  });
+});
+
+function setFlipCardHeight() {
+  document.querySelectorAll('.flip-card-inner').forEach(card => {
+    const front = card.querySelector('.flip-card-front');
+    console.log(front.scrollHeight);
+    const back = card.querySelector('.flip-card-back');
+    console.log(front.scrollHeight);
+    const parent = card.closest('.flip-card');
+    if (parent.classList.contains('flipped')) {
+      card.style.height = back.scrollHeight + 'px';
     } else {
-        x.className = 'navtoggle';
+      card.style.height = front.scrollHeight + 'px';
     }
+  });
 }
